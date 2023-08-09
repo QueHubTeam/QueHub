fdcc1ec9b4cfc0702ab68e3574dd4b805b97651a
Iskandar <eiscocyber@gmail.com>
2023-08-09 17:32:53 +0500

    cleared

--- src/QueHub.DAL/Repositories/Repository.cs
+++ src/QueHub.DAL/Repositories/Repository.cs
@@ -14,26 +14,32 @@ public class Repository<T> : IRepository<T> where T : class
         this.dbContext = dbContext;
         table = dbContext.Set<T>();
     }
+
     public void Add(T entity)
     {
         table.Add(entity);
     }
+
     public void Delete(T entity)
     {
         table.Remove(entity);
     }
+
     public T Select(long id)
     {
         return table.Find(id);
     }
+
     public IQueryable<T> SelectAll()
     {
         return table.AsNoTracking();
     }
+
     public void Update(T entity)
     {
         dbContext.Entry<T>(entity).State = EntityState.Modified;
     }
+
     public void SaveChanges()
     {
         dbContext.SaveChanges();
