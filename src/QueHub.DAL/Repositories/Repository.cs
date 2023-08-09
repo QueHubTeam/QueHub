c6e3b20c74d7591c0e5ecf881c707d6e27d5e6df
Iskandar <eiscocyber@gmail.com>
2023-08-09 17:32:18 +0500

    hehe

--- src/QueHub.DAL/Repositories/Repository.cs
+++ src/QueHub.DAL/Repositories/Repository.cs
@@ -9,38 +9,31 @@ public class Repository<T> : IRepository<T> where T : class
     private readonly QueHubDbContext dbContext;
 
     private readonly DbSet<T> table;
-
     public Repository(QueHubDbContext dbContext)
     {
         this.dbContext = dbContext;
         table = dbContext.Set<T>();
     }
-
     public void Add(T entity)
     {
         table.Add(entity);
     }
-
     public void Delete(T entity)
     {
         table.Remove(entity);
     }
-
     public T Select(long id)
     {
         return table.Find(id);
     }
-
     public IQueryable<T> SelectAll()
     {
         return table.AsNoTracking();
     }
-
     public void Update(T entity)
     {
         dbContext.Entry<T>(entity).State = EntityState.Modified;
     }
-
     public void SaveChanges()
     {
         dbContext.SaveChanges();
