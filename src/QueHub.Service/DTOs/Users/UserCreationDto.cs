b73c5493371178628acb2a3d909535b9f2c5ec5b
Iskandar <eiscocyber@gmail.com>
2023-08-16 04:16:49 +0500

    User DTOs added

---
+++ src/QueHub.Service/DTOs/Users/UserCreationDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QueHub.Service.DTOs.Users;
+
+public class UserCreationDto
+{
+    [MaxLength(25)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [MaxLength(25)]
+    public string LastName { get; set; } = string.Empty;
+
+    [MaxLength(25)]
+    public string UserName { get; set; } = string.Empty;
+
+    public string ImagePath { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
---
+++ src/QueHub.Service/DTOs/Users/UserResultDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QueHub.Service.DTOs.Users;
+
+public class UserResultDto
+{
+    public long Id { get; set; }
+
+    [MaxLength(25)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [MaxLength(25)]
+    public string LastName { get; set; } = string.Empty;
+
+    [MaxLength(25)]
+    public string UserName { get; set; } = string.Empty;
+
+    public string ImagePath { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
