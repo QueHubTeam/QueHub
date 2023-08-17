55c5d95b70b6a8b19c019c9c190b35e8e8fbea87
Iskandar <eiscocyber@gmail.com>
2023-08-17 18:19:48 +0500

    UserService fixed with await

--- src/QueHub.Service/Services/UserService.cs
+++ src/QueHub.Service/Services/UserService.cs
@@ -1,12 +1,133 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using QueHub.DAL.IRepositories;
+using QueHub.Domain.Entity.User;
+using QueHub.Service.DTOs.Users;
+using QueHub.Service.Exceptions.Users;
+using QueHub.Service.Interfaces;
 
 namespace QueHub.Service.Services
 {
-    internal class UserService
+    public class UserService : IUserService
     {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public UserService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public async Task<UserResultDto> CreateUserAsync(UserCreationDto userDto)
+        {
+            var existingUser = await unitOfWork.UserRepository.SelectAsync(u => u.Email == userDto.Email);
+            if (existingUser != null)
+                throw new UserAlreadyExistsException();
+
+            var newUser = mapper.Map<UserEntity>(userDto);
+            await unitOfWork.UserRepository.AddAsync(newUser);
+            await unitOfWork.SaveAsync();
+
+            return mapper.Map<UserResultDto>(newUser);
+        }
+
+        public async Task<UserResultDto> UpdateUserAsync(UserUpdateDto userDto)
+        {
+            var existingUser = await unitOfWork.UserRepository.SelectAsync(u => u.Id == userDto.Id);
+            if (existingUser == null)
+                throw new UserNotFoundException();
+
+            mapper.Map(userDto, existingUser);
+            await unitOfWork.UserRepository.UpdateAsync(existingUser);
+            await unitOfWork.SaveAsync();
+
+            return mapper.Map<UserResultDto>(existingUser);
+        }
+
+        public async Task<bool> DeleteUserAsync(long id)
+        {
+            var user = await unitOfWork.UserRepository.SelectAsync(u => u.Id == id);
+            if (user == null)
+                throw new UserNotFoundException();
+
+            user.IsDeleted = true;
+            await unitOfWork.UserRepository.UpdateAsync(user);
+            await unitOfWork.SaveAsync();
+
+            return true;
+        }
+
+        public async Task<UserResultDto> GetUserByIdAsync(long id)
+        {
+            var user = await unitOfWork.UserRepository.SelectAsync(u => u.Id == id);
+            if (user == null)
+                throw new UserNotFoundException();
+
+            return mapper.Map<UserResultDto>(user);
+        }
+
+        public async Task<IEnumerable<UserResultDto>> GetAllUsersAsync()
+        {
+            var users = unitOfWork.UserRepository.SelectAll();
+            return mapper.Map<IEnumerable<UserResultDto>>(users);
+        }
+
+        public async Task<IEnumerable<UserResultDto>> GetAllUsersByNameAsync(string name)
+        {
+            var users = unitOfWork.UserRepository.SelectAll(u =>
+                u.FirstName.Contains(name) || u.LastName.Contains(name));
+
+            return mapper.Map<IEnumerable<UserResultDto>>(users);
+        }
+
+        public async Task<IEnumerable<UserResultDto>> GetAllUsersByUsernameAsync(string username)
+        {
+            var users = unitOfWork.UserRepository.SelectAll(u => u.UserName == username);
+            return mapper.Map<IEnumerable<UserResultDto>>(users);
+        }
+
+        public async Task<bool> CheckCredentialsAsync(string username, string password)
+        {
+            var user = await unitOfWork.UserRepository.SelectAsync(u => u.UserName == username);
+            if (user == null)
+                return false;
+
+            // Implement password validation logic
+        }
+
+        public async Task<bool> ChangePasswordAsync(long userId, string currentPassword, string newPassword)
+        {
+            var user = await unitOfWork.UserRepository.SelectAsync(u => u.Id == userId);
+            if (user == null)
+                throw new UserNotFoundException();
+
+            // Implement password change logic
+        }
+
+        public async Task<bool> UpdateUserRatingAsync(long userId, long newRating)
+        {
+            var user = await unitOfWork.UserRepository.SelectAsync(u => u.Id == userId);
+            if (user == null)
+                throw new UserNotFoundException();
+
+            user.Rating = newRating;
+            await unitOfWork.UserRepository.UpdateAsync(user);
+            await unitOfWork.SaveAsync();
+
+            return true;
+        }
+
+        public async Task<bool> UpdateUserProfileImageAsync(long userId, string imagePath)
+        {
+            var user = await unitOfWork.UserRepository.SelectAsync(u => u.Id == userId);
+            if (user == null)
+                throw new UserNotFoundException();
+
+            user.ImagePath = imagePath;
+            await unitOfWork.UserRepository.UpdateAsync(user);
+            await unitOfWork.SaveAsync();
+
+            return true;
+        }
     }
 }
