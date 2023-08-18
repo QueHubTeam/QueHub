02e1ccf59fc1c432d91f8cc340cda39935b20b54
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-08-18 19:19:58 +0500

    Notification has been added to the service

---
+++ src/QueHub.Service/Interfaces/Notifications/ISmsSender.cs
@@ -0,0 +1,8 @@
+﻿using QueHub.Service.DTOs.Notifications;
+
+namespace QueHub.Service.Interfaces.Notifications;
+
+public interface ISmsSender
+{
+    public Task<bool> SendAsync(SmsMessage smsMessage);
+}
